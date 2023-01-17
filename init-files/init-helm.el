aad5bdb1ffa703dfc436a789aac43c63956375af
Xin Li <xinii@msn.com>
2023-01-17 09:51:45 +0900

    fix: remove the dependency on helm-config to adapt to the new version of helm

--- init-files/init-helm.el
+++ init-files/init-helm.el
@@ -1,5 +1,5 @@
 (require 'helm)
-(require 'helm-config)
+;; (require 'helm-config) ; for old version of helm
 
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
 ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
